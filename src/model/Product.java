c6c6ded4b5459c4d3c24f97fe1103374ce08c27e
Sylwester Wojcik <sylwek3455@gmail.com>
2024-01-13 11:18:12 +0100

    finish needed

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Product {
     private String name;
     private double price;
@@ -15,5 +17,18 @@ public class Product {
 
     public double getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
