fbf7006a08cac63255324b82908fe5179165cdeb
Sylwester Wojcik <sylwek3455@gmail.com>
2024-01-17 22:42:32 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
+    /*
+Napisz krótki program do zarządzania sklepem internetowym.
+W sklepie pracuje obsługa(imie, nazwisko, adres, data rozpoczęcia pracy). Osoby z obsługi mogą zostać managerami.
+Sklep może zarejestrować nowego klienta(imie, nazwisko, adres, data rejestracji). Klient może mieć wiele koszyków,
+ 	które mogą przechowywać wiele produktów(nazwa, cena).
+W sklepie powinna być możliwość dokonania zakupu, co usuwa koszyk klientowi,
+ 	ale umieszcza jego odpowiednik w "historii zamówień".
+*/
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
