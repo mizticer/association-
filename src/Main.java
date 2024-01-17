654796b9f886c7ba57b5c73d28be00af13d64754
Sylwester Wojcik <sylwek3455@gmail.com>
2024-01-17 19:27:29 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
+    /*
+		 *  W systemie przechowujemy informacje o zwierzetach (nazwa, gatunek). Dla psów przechowujemy dodatkowe ich dlugosc siersci
+			oraz ulubione zabawki ,dla kotów stopien bycia wkurwiajacym futrzakiem (1-10) oraz dla pterodaktylow rozpietosc skrzydel.
+
+			Trenerzy (imie, nazwisko, adres) trenuja zwierzeta. Kazdy trener trenuje wiele zwierzat, ale zwierze jest pod okiem jednego trenera.
+
+			Trener uzywa Sprzetu (nazwa, stopien zuzycia(nowe, uzywane, tragiczne)), sprzet moze byc naprawiany.
+			 Chcemy przechowywac informacje o tym jaki trener uzywal kiedy
+			jkaiego sprzetu. Sprzet przez trenera moze byc uzywany wielokrotnie.
+
+			-znajdz trenera ktory trenował najwiecej zwierzat
+			-znajdz trenera ktory uzywał jakiegos sprzetu najwieksza liczbe razy
+			-znajdz trenerow ktorzy trenowali psy z gatunku spaniel
+			-znajdz wszystkie sprzety które byly naprawiane co najmniej 2 razy
+			-znajdz wszystkie pterodaktyle ktore nigdy nie mialy przypisanego trenera
+		 */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
