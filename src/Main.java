92359c26a7c483b345d577f2af06c07c8af8ff8f
Sylwester Wojcik <sylwek3455@gmail.com>
2024-01-18 17:06:34 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    /*
+Napisz program do zarządzania kursami.
+Nauczyciel ma imię, nazwisko, datę zatrudnienia i może uczyć wielu języków
+Student ma imię, nazwisko, język którego się uczy.
+Student może mieć jednego nauczyciela
+Nauczyciel może mieć wielu studentów
+
+zbuduj program tak by dało się dodawać lekcje (termin)
+
+-przypisywanie kursantowi Nauczyciela musi weryfikować język
+-lekcja nie może być dodawana w przeszłości.
+-znajdz kursanta, który ma najwięcej lekcji
+-znajdz nauczyciela, który ma najwięcej kursantów
+
+*/
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
